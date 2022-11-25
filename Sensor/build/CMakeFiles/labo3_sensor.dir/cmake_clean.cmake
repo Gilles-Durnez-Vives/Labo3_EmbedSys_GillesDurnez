4f9d7c4d2d92c3c7418dbe5908c6a2e3c5013eb2
Gilles-Durnez-Vives <gilles.durnez@student.vives.be>
2022-11-25 15:09:16 +0100

    L3B0.1 -- I2C added

---
+++ Sensor/build/CMakeFiles/labo3_sensor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labo3_sensor.dir/sensor.c.o"
+  "labo3_sensor"
+  "labo3_sensor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/labo3_sensor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Sensor/sensor.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>				//Needed for I2C port
+#include <fcntl.h>				//Needed for I2C port
+#include <sys/ioctl.h>			//Needed for I2C port
+#include <linux/i2c-dev.h>		//Needed for I2C port
+
+
+int main()
+{
+    int file_i2c;
+	int length;
+    int addr = 0x4a;          //<<<<<The I2C address of the slave
+    char *filename = (char*)"/dev/i2c-1";  //Filelocation of i2c data on py
+	unsigned char buffer[60] = {0};
+	
+	//----- OPEN THE I2C BUS -----	
+	if ((file_i2c = open(filename, O_RDWR)) < 0)
+	{
+		//ERROR HANDLING: you can check errno to see what went wrong
+		printf("Failed to open the i2c bus");
+		return 1;
+	}
+	
+	
+	if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
+	{
+		printf("Failed to acquire bus access and/or talk to slave.\n");
+		//ERROR HANDLING; you can check errno to see what went wrong
+		return 1;
+	}
+	
+	
+	//----- READ BYTES -----
+	length = 4;			//<<< Number of bytes to read
+	if (read(file_i2c, buffer, length) != length)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
+	{
+		//ERROR HANDLING: i2c transaction failed
+		printf("Failed to read from the i2c bus.\n");
+	}
+	else
+	{
+		printf("Data read: %8x\n", buffer[0]);
+	}
+
+    return 0;
+}
